4471618bd9b80539338a4f7a1be2d0a1dc390502
Andrew Tsai <tsai.tsunghua@gmail.com>
2023-05-24 17:52:47 +0800

    Add plot_skeleton_3d().

--- examples/detect_pose.py
+++ examples/detect_pose.py
@@ -19,12 +19,11 @@ import imageio.v2 as imageio
 import imageio.v3 as iio
 import matplotlib.pyplot as plt
 
-from skeleton_utils import re_order
-from skeleton_utils import convert_holistic_to_skeleton_2d
-from skeleton_utils import convert_to_skeleton_3d_h36m17p, scale_skeleton_3d_from_h36m
-from cascade_detector import CascadeDetector
-from plot_utils import plot_skeleton_2d
-from plot_utils import draw_skeleton, plot_3d_ax, adjust_figure
+from skyeye.pose_estimation.evo import CascadeDetector
+from skyeye.pose_estimation.evo import convert_holistic_to_skeleton_2d
+from skyeye.pose_estimation.evo import convert_to_skeleton_3d_h36m17p, scale_skeleton_3d_from_h36m
+from skyeye.pose_estimation.evo import plot_skeleton_2d, plot_skeleton_3d
+from skyeye.pose_estimation.evo import plot_3d_ax
 
 
 from skyeye.utils.opencv import Webcam, wait_key
@@ -93,10 +92,15 @@ def main():
     with open(r'mediapipe.yaml', 'r', encoding='utf-8') as f:
         inputs = yaml.load(f, Loader=yaml.Loader)
 
-    enable_gpu = inputs['enable_gpu']
+    use_gpu = inputs['use_gpu']
+
+    if use_gpu == 1:
+        use_gpu = True
+    else:
+        use_gpu = False
+
 
     if use_webcam:
-
         webcam = Webcam()
         if webcam.is_open():
             webcam.release()
@@ -117,7 +121,7 @@ def main():
     stats_path = './stats.npy'
     
     # Detector
-    detector = CascadeDetector()
+    detector = CascadeDetector(use_gpu=use_gpu)
     detector.load_model(model_path)
     detector.load_stats(stats_path)
     
@@ -152,13 +156,8 @@ def main():
         image = frame.copy()
         image_out = frame.copy()
 
-        # Flip the image horizontally for a later selfie-view display, and convert
-        if enable_gpu == 1:
-            # the BGR image to RGBA.
-            image = cv.cvtColor(image, cv.COLOR_BGR2RGBA)
-        else:
-            # the BGR image to RGB.
-            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        # Convert to RGB format
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
@@ -168,7 +167,6 @@ def main():
         holistic_data.update(results)
         skel_2d = convert_holistic_to_skeleton_2d(holistic_data)
 
-        #f = plt.figure()
         f = plt.figure(figsize=(15, 5))
 
         ax1 = plt.subplot(131)
@@ -176,12 +174,13 @@ def main():
         ax1.imshow(image)
 
         ax2 = plt.subplot(132)
-        plt.title('2D key-point inputs: {:d}*2'.format(num_joints))
-        ax2.set_aspect('equal')
-        ax2.invert_yaxis()
-        plot_skeleton_2d(ax2, skel_2d)
+        #plt.title('2D key-point inputs')
+        #ax2.set_aspect('equal')
+        #ax2.invert_yaxis()
+        plot_skeleton_2d(ax2, skel_2d,
+            title="2D key-point inputs")
         #draw_skeleton(ax2, skel_2d, gt=True)
-        plt.plot(skel_2d[:,0], skel_2d[:,1], 'ro', 2)       
+        #plt.plot(skel_2d[:,0], skel_2d[:,1], 'ro', 2)       
 
 
         timer.tic()
@@ -195,16 +194,17 @@ def main():
         skel_3d = skel_3d.reshape(-1, 3)
 
         skel_3d = convert_to_skeleton_3d_h36m17p(skel_3d)
-        #skel_3d = scale_skeleton_3d_from_h36m(skel_3d, frame_width, frame_height)
 
         print(f"skel_2d shape: {skel_2d.shape}")
         print(f"skel_3d shape: {skel_3d.shape}")
-        #print(f"hip_2d, hip_3d: {skel_2d[0]}, {skel_3d[0]}")
-        #print(f"left_wrist_2d, left_wrist_3d: {skel_2d[13]}, {skel_3d[19]}")
-        #print(f"right_wrist_2d, right_wrist_3d: {skel_2d[16]}, {skel_3d[27]}")
 
 
         ax3 = plt.subplot(133, projection='3d')
+
+        plot_skeleton_3d(ax3, skel_3d, elev=90, azim=90,
+             xlabel="x", ylabel="y", zlabel="z", title="3d prediction")
+
+        '''
         plot_3d_ax(ax=ax3, 
                    pred=pred, 
                    elev=0,  # 10, 
@@ -218,6 +218,7 @@ def main():
                       wspace = 0.3, 
                       hspace = 0.3
                       )       
+        '''
 
         # Save figure
         filename = f"snapshot.jpg" 
@@ -232,21 +233,12 @@ def main():
         ax1.imshow(image)
 
         ax2 = plt.subplot(132)
-        plt.title('2D key-point inputs: {:d}*2'.format(num_joints))
-        ax2.set_aspect('equal')
-        ax2.invert_yaxis()
-        plot_skeleton_2d(ax2, skel_2d)
-        #draw_skeleton(ax2, skel_2d, gt=True)
-        plt.plot(skel_2d[:,0], skel_2d[:,1], 'ro', 2)       
+        plot_skeleton_2d(ax2, skel_2d,
+            title="2D key-point inputs")
 
-        skel_3d_to_2d = skel_3d[:,0:2]
-
+        skel_3d_to_2d = skel_3d[:, 0:2]
         ax3 = plt.subplot(133)
-        plt.title(f"skel_3d to 2d")
-        ax3.set_aspect('equal')
-        ax3.invert_yaxis()
-        plot_skeleton_2d(ax3, skel_3d_to_2d)
-        plt.plot(skel_3d_to_2d[:,0], skel_3d_to_2d[:,1], 'ro', 2)       
+        plot_skeleton_2d(ax3, skel_3d_to_2d, title="skel_3d to 2d")
 
 
         # Save figure
